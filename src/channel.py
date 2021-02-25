3aa7114b61b10a93b741420632d538893814fe6b
Terrance <z5309029@ad.unsw.edu.au>
2021-02-25 19:45:18 +1100

    Added comments describing each function for iteration_1

--- src/channel.py
+++ src/channel.py
@@ -1,7 +1,36 @@
+'''
+Function:
+    Invites a user (with user id u_id) to join a channel with ID channel_id. 
+    Once invited the user is added to the channel immediately.
+
+Return Type:
+    {}
+
+Exceptions:
+    InputError when any of:
+        - channel_id does not refer to a valid channel.
+        - u_id does not refer to a valid user.
+    AccessError when any of:
+        - the authorised user is not already a member of the channel.
+'''
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     return {
     }
 
+'''
+Function:
+    Given a Channel with ID channel_id that the authorised user is part of, 
+    provide basic details about the channel.
+
+Return Type:
+    { name, owner_members, all_members }
+
+Exceptions:
+    InputError when any of:
+        - Channel ID is not a valid channel.
+    AccessError when any of:
+        - Authorised user is not a member of channel with channel_id.
+'''
 def channel_details_v1(auth_user_id, channel_id):
     return {
         'name': 'Hayden',
@@ -21,6 +50,25 @@ def channel_details_v1(auth_user_id, channel_id):
         ],
     }
 
+'''
+Function:
+    Given a Channel with ID channel_id that the authorised user is part of, 
+    return up to 50 messages between index "start" and "start + 50". Message 
+    with index 0 is the most recent message in the channel. This function 
+    returns a new index "end" which is the value of "start + 50", or, if this 
+    function has returned the least recent messages in the channel, returns -1 
+    in "end" to indicate there are no more messages to load after this return.
+
+Return Type:
+    { messages, start, end }
+
+Exceptions:
+    InputError when any of:
+        - Channel ID is not a valid channel.
+        - Start is greater than the total number of messages in the channel.
+    AccessError when any of:
+        - Authorised user is not a member of channel with channel_id.
+'''
 def channel_messages_v1(auth_user_id, channel_id, start):
     return {
         'messages': [
@@ -39,6 +87,21 @@ def channel_leave_v1(auth_user_id, channel_id):
     return {
     }
 
+'''
+Function:
+    Given a channel_id of a channel that the authorised user can join, adds them 
+    to that channel
+
+Return Type:
+    {}
+
+Exceptions:
+    InputError when any of:
+        - Channel ID is not a valid channel.
+    AccessError when any of:
+        - channel_id refers to a channel that is private (when the authorised 
+          user is not a global owner).
+'''
 def channel_join_v1(auth_user_id, channel_id):
     return {
     }
