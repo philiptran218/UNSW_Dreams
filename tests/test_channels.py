d2e60d07effeac06dafb4ec69e56cb2c0ea207b9
John Aspite <z5258954@ad.unsw.edu.au>
2021-03-01 20:36:46 +1100

    Adding the file to the repo, see if it works

---
+++ tests/test_channels.py
@@ -0,0 +1,11 @@
+from src.channels import channels_create_v1
+from src.auth import auth_register_v1
+import pytest
+from src.error import InputError
+
+def test_channels_list():
+    pass
+
+
+def test_channels_listall():
+    pass
