0d7177156b7515a5bf5384b23d32393bf64bd8b5
ribal albalkhi <z5260369@ad.unsw.edu.au>
2021-03-02 11:38:42 +1100

    rearranged

--- tests/test_channels.py
+++ tests/test_channels.py
@@ -8,13 +8,15 @@ from src.other import clear_v1
 def clear_data():
     clear_v1()
 
-def test_channels_create_v1(clear_data):
+def test_valid_channels_create_v1(clear_data):
     #created a user_id by regestring a valid user
     userId = auth_register_v1("validemail@g.com", "validpass", "validname","validname")
-    
     #Valid case
     assert(channels_create_v1(userId, "ValidChannelName", True) == {'channel_id': 1})
     
+def test_invalidName_channels_create_v1(clear_data):
+    #created a user_id by regestring a valid user
+    userId = auth_register_v1("validemail@g.com", "validpass", "validname","validname")
     #channel name is more than 20 characters long
     invalidName = "nameismorethantwentycharacters"
     with pytest.raises(InputError):
