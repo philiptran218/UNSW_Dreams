8863800af2d2440bce3c27ce4597bfd4fea014f3
Philip Tran <philip.tran@student.unsw.edu.au>
2021-03-06 14:59:58 +1100

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -113,7 +113,7 @@ def auth_register_v1(email, password, name_first, name_last):
         'name_last': name_last,
         'perm_id': perm_id,
         'password': password,
-        'handle': generate_handle(name_first, name_last)
+        'handle': generate_handle(name_first, name_last),
         'email': email
     }
     
