4ddf66a12b36e13e7ecebe9632c7ac2951694d16
z5309034 <z5309034@cse.unsw.edu.au>
2021-03-06 13:57:34 +1100

    Cleaned up comments

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -5,229 +5,168 @@ from src.error import InputError, AccessError
 from src.auth import auth_register_v1
 from src.channels import channels_create_v1
 from src.message import message_send_v1
-# from src.data import data
+from src.other import clear_v1
 
 @pytest.fixture
-def supply_user1():
-    
-    ''' this user is a global owner '''
+def user1():
     new_user1 = auth_register_v1('johnsmith@gmail.com', 'goodpass', 'John', 'Smith')    
-    return new_user1
+    return new_user1['auth_user_id']
     
 @pytest.fixture
-def supply_user2():
-    
+def user2():
     new_user2 = auth_register_v1('philt@gmail.com', 'badpass', 'Phil', 'Tran')
-    return new_user2
+    return new_user2['auth_user_id']
     
 @pytest.fixture
-def supply_user3():
-    
+def user3():
     new_user3 = auth_register_v1('person@gmail.com', 'helloworld', 'First', 'Name')
-    return new_user3
+    return new_user3['auth_user_id']
   
 @pytest.fixture
-def supply_chan1(supply_user1):   
-
-    ''' channel1 is public ''' 
-    new_chan1 = channels_create_v1(supply_user1, 'channel1', True) 
-    return new_chan1
+def channel1(user1):   
+    # This channel is public, created by user1
+    new_chan1 = channels_create_v1(user1, 'channel1', True) 
+    return new_chan1['channel_id']
 
 @pytest.fixture    
-def supply_chan2(supply_user2):
-    
-    ''' channel2 is private '''
-    new_chan2 = channels_create_v1(supply_user2, 'channel2', False) 
-    return new_chan2
+def channel2(user2):
+    # This channel is private, created by user2
+    new_chan2 = channels_create_v1(user2, 'channel2', False) 
+    return new_chan2['channel_id']
 
 @pytest.fixture
-def supply_message1(supply_user1, supply_chan1):
-    
-    message_send_v1(supply_user1, supply_chan1, 'A new message')
+def supply_message1(user1, channel1):
+    # Sends a single message to channel1
+    message_send_v1(user1, channel1, 'A new message')
 
 @pytest.fixture    
-def supply_multi1(supply_user1, supply_chan1, supply_message1):
-
+def supply_multi1(user1, channel1, supply_message1):
+    # Sends 55 messages to channel1, the messages are just numbers
     i = 1
     while i <= 55:
-        message_send_v1(supply_user1, supply_chan1, f'{i}')
+        message_send_v1(user1, channel1, f"{i}")
         i += 1
         
-    
-    ''' data added from fixtures should look like this '''
-'''
-    data = {
-        'users': [
-            {
-                'u_id': 1,
-                'password': 'goodpass'
-                'first_name': 'John',
-                'last_name': 'Smith',
-                'email': 'johnsmith@gmail.com',
-                'perm_id': 1,
-            },  
-            {
-                'u_id': 2,
-                'password': 'badpass'
-                'first_name': 'Phil',
-                'last_name': 'Tran',
-                'email': 'philt@gmail.com',
-                'perm_id': 2,
-            },
-        ],
-        'channels': [   
-            {
-                'channel_id': 1,
-                'name': 'channel1',
-                'owner_ids': [1],
-                'member_ids': [1],
-                'messages': [
-                    {          
-                        'message_id': 1,
-                        'u_id': 1,
-                        'message': 'A new message',
-                        'time_created': this can be whatever.
-                    }
-                ],    
-                'is_public': True 
-            },
-            {
-                'channel_id': 2,
-                'name': 'channel2',
-                'owner_ids': [2],
-                'member_ids': [2],
-                'messages': [
-                    currently blank
-                ],    
-                'is_public': False   
-            }
-        ]
-    }
-'''
+@pytest.fixture
+def clear_database():
+    clear_v1()
+   
                           
 ################################################################################
 # channel_messages_v1 tests
 ################################################################################
     
-def test_channel_messages_invalid_channel(clear_v1, supply_user1, supply_chan1):
-    #clear_v1()
+def test_channel_messages_invalid_channel(clear_database, user1, channel1):
+    # Raises InputError since channel_id 123456 does not exist
     with pytest.raises(InputError):
-        channel_messages_v1(supply_user1, 123456, 0) 
-        ''' channel_id 123456 doesnt exist '''
+        channel_messages_v1(user1, 123456, 0) 
         
         
-def test_channel_messages_invalid_start(supply_user2, supply_chan2):
-    
+def test_channel_messages_invalid_start(clear_database, user2, channel2):
+    # Raises InputError since start is greater than num messages in channel2
+    # (there are no messages in channel2)
     with pytest.raises(InputError):
-        channel_messages_v1(supply_user2, supply_chan2, 10) 
-        ''' raises error since start is 10, but there are no messages in supply_chan2 '''
+        channel_messages_v1(user2, channel2, 10) 
     
 
-def test_channel_messages_authid_not_member(supply_user1, supply_chan1, supply_user2):
-    
+def test_channel_messages_authid_not_member(clear_database, user1, channel1, user2):
+    # Raises AccessError since user2 is not a member of channel1
     with pytest.raises(AccessError):
-        channel_messages_v1(supply_user2, supply_chan1, 0) 
-        ''' fails since supply_user2 is not in supply_chan1 '''
+        channel_messages_v1(user2, channel1, 0) 
         
                
-def test_channel_messages_invalid_authid(supply_user1, supply_chan1):
-
+def test_channel_messages_invalid_authid(clear_database, user1, channel1):
+    # Raises AccessError since u_id 123456 does not exist
     with pytest.raises(AccessError):
-        channel_messages_v1(123456, supply_chan1, 0) 
-        ''' auth_id 123456 doesnt exist '''
+        channel_messages_v1(123456, channel1, 0) 
         
         
-def test_channel_messages_valid_single(supply_user1, supply_chan1, supply_message1):
-
-    message_detail = channel_messages_v1(supply_user1, supply_chan1, 0)
+def test_channel_messages_valid_single(clear_database, user1, channel1, supply_message1):
+    # Tests for a single message in channel1
+    message_detail = channel_messages_v1(user1, channel1, 0)
     
-    ''' checking each value is correct in the returned dictionary '''
+    # Checking the message dictionary to see if message has been appended
     assert message_detail['messages'][0]['message_id'] == 1
-    assert message_detail['messages'][0]['u_id'] == supply_user1
+    assert message_detail['messages'][0]['u_id'] == user1
     assert message_detail['messages'][0]['message'] == 'A new message'
     assert message_detail['start'] == 0
     assert message_detail['end'] == -1
     
    
-def test_channel_messages_multiple(supply_user1, supply_chan1, supply_message1, supply_multi1):
-
-    message_detail = channel_messages_v1(supply_user1, supply_chan1, 0)
-    
-    assert message_detail['messages'][0]['message_id'] == 1
-    assert message_detail['messages'][0]['u_id'] == supply_user1
-    assert message_detail['messages'][0]['message'] == 'A new message'
-    
-    i = 1
-    while i <= 50:
-        assert message_detail['messages'][i]['message_id'] == i + 1
-        assert message_detail['messages'][i]['u_id'] == supply_user1
-        assert message_detail['messages'][i]['message'] == f'{i}'
+def test_channel_messages_multiple(clear_database, user1, channel1, supply_multi1):
+    # Testing for multiple messages, and non-zero start value
+    message_detail = channel_messages_v1(user1, channel1, 2)
+   
+    # Checking that the messages have been appended correctly
+    i = 53
+    j = 0
+    while i >= 4:
+        assert message_detail['messages'][j]['message_id'] == i
+        assert message_detail['messages'][j]['u_id'] == user1
+        assert message_detail['messages'][j]['message'] == str(i)
+        i -= 1
+        j += 1
         
-    assert message_detail['start'] == 0
-    assert message_detail['end'] == 50
-
+    assert message_detail['start'] == 2
+    assert message_detail['end'] == 52
     
     
 ################################################################################
 # channel_join_v1 tests
 ################################################################################
     
-def test_channel_join_invalid_authid(supply_user1, supply_user2, supply_chan1):
-
+def test_channel_join_invalid_authid(clear_database, user1, channel1):
+    # Raises AccessError since auth_user_id 123456 does not exist
     with pytest.raises(AccessError):
-        channel_join_v1(123456, supply_chan1) 
-        ''' fails since auth_user_id 123456 doesnt exist '''
-        
+        channel_join_v1(123456, channel1) 
+                
     
-def test_channel_join_invalid_channel(supply_user1, supply_chan1):
-
+def test_channel_join_invalid_channel(clear_database, user1, channel1):
+    # Raises InputError since channel_id 2 does not exist
     with pytest.raises(InputError):
-        channel_join_v1(supply_user1, 2) 
-        ''' should fail since channel_id 2 does not exist'''
+        channel_join_v1(user1, 2) 
         
         
-def test_channel_join_private_channel(supply_user2, supply_chan2, supply_user3):        
-        
+def test_channel_join_private_channel(clear_database, user2, channel2, user3):        
+    # Raises AccessError since user3 is a member attempting to enter a private
+    # channel
     with pytest.raises(AccessError):    
-        channel_join_v1(supply_user3, supply_chan2) 
-        ''' should fail since supply_chan2 is private and supply_user3 is not a global owner '''
+        channel_join_v1(user3, channel2) 
         
         
-def test_channel_join_valid(supply_user1, supply_chan1, supply_user2):
-    channel_join_v1(supply_user2, supply_chan1)
+def test_channel_join_valid(clear_database, user1, channel1, user2):
+    # Testing if a single member can join a public channel
+    channel_join_v1(user2, channel1)
     
-    channels = channel_details_v1(supply_user1, supply_chan1)
-    
-    ''' ensuring that number of members increases and their names are listed in order '''
+    channels = channel_details_v1(user1, channel1)
     assert len(channels['all_members']) == 2
-    assert channels['all_members'][0]['u_id'] == supply_user1
-    assert channels['all_members'][1]['u_id'] == supply_user2    
+    assert channels['all_members'][0]['u_id'] == user1
+    assert channels['all_members'][1]['u_id'] == user2    
     
     
-def test_channel_join_valid_multi(supply_user1, supply_user2, supply_user3, supply_chan1):
+def test_channel_join_valid_multi(clear_database, user1, user2, user3, channel1):
+    # Testing if multiple members can join a public channel
+    channel_join_v1(user2, channel1)
+    channel_join_v1(user3, channel1)
 
-    channel_join_v1(supply_user2, supply_chan1)
-    channel_join_v1(supply_user3, supply_chan1)
-
-    channels = channel_details_v1(supply_user1, supply_chan1)
-    
+    channels = channel_details_v1(user1, channel1)
     assert len(channels['all_members']) == 3
-    assert channels['all_members'][0]['u_id'] == supply_user1
-    assert channels['all_members'][1]['u_id'] == supply_user2
-    assert channels['all_members'][2]['u_id'] == supply_user3
+    assert channels['all_members'][0]['u_id'] == user1
+    assert channels['all_members'][1]['u_id'] == user2
+    assert channels['all_members'][2]['u_id'] == user3
     
 
-def test_channel_join_global_private(supply_user1, supply_user2, supply_chan2):
-
-    ''' should allow global owner to join private channel '''
-    channel_join_v1(supply_user1, supply_chan2)
+def test_channel_join_global_private(clear_database, user1, user2, channel2):
+    # Test to see if a global member (user1), gets added as a member and owner
+    # of channel2
+    channel_join_v1(user1, channel2)
     
-    channels = channel_details_v1(supply_user2, supply_chan2)
-    
+    channels = channel_details_v1(user2, channel2)
+    # Checking if user1 has been added into the members and owners list 
     assert len(channels['all_members']) == 2
     assert len(channels['owner_members']) == 2
-    assert channels['all_members'][0]['u_id'] == supply_user2
-    assert channels['all_members'][1]['u_id'] == supply_user1
-    assert channels['owner_members'][0]['u_id'] == supply_user2
-    assert channels['owner_members'][1]['u_id'] == supply_user1
+    assert channels['all_members'][0]['u_id'] == user2
+    assert channels['all_members'][1]['u_id'] == user1
+    assert channels['owner_members'][0]['u_id'] == user2
+    assert channels['owner_members'][1]['u_id'] == user1
         
