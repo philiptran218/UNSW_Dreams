db1a8fbf2a379baf69ca9ddf8491237a375c80d6
Vivien Tang <vivien.tang@student.unsw.edu.au>
2021-03-06 15:15:08 +1100

    Update helper.py

--- src/helper.py
+++ src/helper.py
@@ -16,8 +16,8 @@ def generate_handle(name_first, name_last):
         if handle_num >= 1:
             handle = handle.replace(handle[len(handle) - 1], "")
         
-            handle = handle + str(handle_num)
-            handle_num += 1
+        handle = handle + str(handle_num)
+        handle_num += 1
 
         return handle
 
