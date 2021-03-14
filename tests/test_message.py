b40b906d9349c04ea1bc55d3f13c9ce8bb3bf3da
z5309034 <z5309034@cse.unsw.edu.au>
2021-03-14 14:40:45 +1100

    Added a fixture for sending a single message and added a new test for removing a valid message

--- tests/test_message.py
+++ tests/test_message.py
@@ -25,34 +25,40 @@ def channel1(user1):
 def clear_database():
     clear_v1()
     
-def test_message_remove_deleted_message(clear_database, user1, channel1): 
+@pytest.fixture
+def message1(user1, channel1):
     msgid = message_send_v1(user1, channel1, 'Hello World')
-    message_remove_v1(user1, msgid['message_id'])
+    return msgid['message_id']
+    
+def test_message_remove_deleted_message(clear_database, user1, channel1, message1):  
+    message_remove_v1(user1, message1)
     with pytest.raises(InputError):
-        message_remove_v1(user1, msgid['message_id'] )
+        message_remove_v1(user1, message1)
                
-def test_message_remove_accesserror(clear_database, user1, channel1, user2):
-    msgid = message_send_v1(user1, channel1, 'Hello World')
+def test_message_remove_accesserror(clear_database, user1, channel1, user2, message1):
     with pytest.raises(AccessError):
-        message_remove_v1(user2, msgid['message_id'])
+        message_remove_v1(user2, message1)
      
-def test_message_remove_accesserror2(clear_database, user1, channel1, user2):
+def test_message_remove_accesserror2(clear_database, user1, channel1, user2, message1):
     # This test is similar to previous test, but user2 is now a member of 
     # channel1
-    msgid = message_send_v1(user1, channel1, 'Hello World')
     channel_join_v1(user2, channel1)
     with pytest.raises(AccessError):
-        message_remove_v1(user2, msgid['message_id'])
+        message_remove_v1(user2, message1)
         
-def test_message_remove_uid_does_not_exist(clear_database, user1, channel1):
-    msgid = message_send_v1(user1, channel1, 'Hello World')
+def test_message_remove_uid_does_not_exist(clear_database, user1, channel1, message1):
     with pytest.raises(AccessError):
-        message_remove_v1(1000, msgid['message_id'])
+        message_remove_v1(1000, message1)
         
 # Assuming InputError raised when message_id does not exist
 def test_message_remove_invalid_messageid(clear_database, user1, channel1):
-    msgid = message_send_v1(user1, channel1, 'Hello World')
     with pytest.raises(InputError):
         message_remove_v1(user1, 1000)
         
+# Assuming that removed messages will no longer appear in channel
+def test_message_remove_valid_single(clear_database, user1, channel1, message1):
+    message_remove_v1(user1, message1)
+    assert channel_messages_v1(user1, channel1, 0) == {'messages': [], 'start': 0, 'end': -1}
+
+# Also write another test for removing from dm and channel simultaneously        
 
