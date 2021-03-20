2930c1446da0761f8ab2d2a776e7db29391d401a
z5309034 <z5309034@cse.unsw.edu.au>
2021-03-20 16:14:06 +1100

    Added helper function to check if message string is empty

--- src/message.py
+++ src/message.py
@@ -3,6 +3,12 @@ from src.database import data
 import src.helper as helper
 from datetime import timezone, datetime
 
+def is_message_empty(message):
+    message = message.replace(' ', '')
+    message = message.replace('\n', '')
+    message = message.replace('\t', '')
+    return len(message) == 0
+
 def message_send_v1(auth_user_id, channel_id, message):
     # Check for valid u_id
     if helper.is_valid_uid(auth_user_id) == False:
@@ -14,7 +20,7 @@ def message_send_v1(auth_user_id, channel_id, message):
     if helper.is_already_in_channel(auth_user_id, channel_id) == False:
         raise AccessError("User is not a member in the channel they are sending the message to")
     # Check if message is empty
-    if message == '':
+    if is_message_empty(message):
         raise InputError("Empty messages cannot be posted to channels")
     # Check if message surpasses accepted length
     if len(message) > 1000:
