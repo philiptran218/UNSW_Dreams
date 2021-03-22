56cb03c6e23c5773969fc194b87704204854f12f
z5309034 <z5309034@cse.unsw.edu.au>
2021-03-22 17:59:39 +1100

    Improved comments and used constants in tests

--- tests/test_message.py
+++ tests/test_message.py
@@ -25,15 +25,29 @@ def channel1(user1):
 def clear_database():
     clear_v1()
 
+INVALID_ID = -1
+
+################################################################################
+# message_send_v1 tests                                                        #
+################################################################################
+
 def test_message_send_invalid_uid(clear_database, channel1):
+    # Raises AccessError since u_id INVALID_ID does not exist
     with pytest.raises(AccessError):
-        message_send_v1(1000, channel1, 'Hello World')
+        message_send_v1(INVALID_ID, channel1, 'Hello World')
+        
+def test_message_send_invalid_channel(clear_database, user1):
+    # Raises InputError since channel_id INVALID_ID does not exist
+    with pytest.raises(InputError):
+        message_send_v1(user1, INVALID_ID, 'Nice to meet you!')
         
 def test_message_send_uid_not_in_channel(clear_database, channel1, user2):
+    # Raises AccessError since user2 is not in channel1
     with pytest.raises(AccessError):
         message_send_v1(user2, channel1, 'Hello World')
         
 def test_message_send_invalid_messages(clear_database, user1, channel1):
+    # Raises InputError since message > 1000 characters
     i = 0
     message = ''
     while i < 500:
@@ -42,17 +56,14 @@ def test_message_send_invalid_messages(clear_database, user1, channel1):
     with pytest.raises(InputError):
         message_send_v1(user1, channel1, message)
         
-def test_message_send_invalid_channel(clear_database, user1):
-    with pytest.raises(InputError):
-        message_send_v1(user1, 1000, 'Nice to meet you!')
-        
-# Assuming that empty messages will not be valid/code will not add an empty message
-# which is either blank or only contains whitespace
+# Assuming that function will not add empty messages (or messages with just
+# whitespace) and instead raises InputError 
 def test_message_send_empty_message(clear_database, user1, channel1):
     with pytest.raises(InputError):
         message_send_v1(user1, channel1, '')
         
 def test_message_send_single_message(clear_database, user1, channel1):
+    # Tests sending a single message to channel
     msgid = message_send_v1(user1, channel1, 'Hello World')
     
     channel_messages = channel_messages_v1(user1, channel1, 0)['messages']
@@ -61,4 +72,26 @@ def test_message_send_single_message(clear_database, user1, channel1):
     assert channel_messages[0]['u_id'] == user1
     assert channel_messages[0]['message'] == 'Hello World'
 
+def test_message_send_multi_messages(clear_database, user1, channel1): 
+    # Testing for multiple messages
+    # Sends 55 messages to channel, the messages are just numbers as strings
+    i = 1
+    while i <= 55:
+        message_send_v1(user1, channel1, f"{i}")
+        i += 1
+    
+    message_detail = channel_messages_v1(user1, channel1, 2)
+    # Checking that the messages have been appended correctly
+    i = 53
+    j = 0
+    while i >= 4:
+        assert message_detail['messages'][j]['message_id'] == i
+        assert message_detail['messages'][j]['u_id'] == user1
+        assert message_detail['messages'][j]['message'] == str(i)
+        i -= 1
+        j += 1 
+    assert message_detail['start'] == 2
+    assert message_detail['end'] == 52
+
+# Add test with multiple senders
 
