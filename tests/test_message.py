567c65de0b9288f2705dc64982fab3509151412f
z5309034 <z5309034@cse.unsw.edu.au>
2021-03-23 12:17:30 +1100

    Added comments to each test and added an extra test for whitespace messages

--- tests/test_message.py
+++ tests/test_message.py
@@ -2,9 +2,7 @@ import pytest
 from src.error import InputError, AccessError 
 from src.auth import auth_register_v1
 # Change to import dm once created
-from src.channels import channels_create_v1
 from src.other import clear_v1
-from src.channel import channel_messages_v1
 from src.message import message_senddm_v1
 
 INVALID_ID = -1
@@ -28,15 +26,23 @@ def dm1(user1):
 def clear_database():
     clear_v1()
 
-def test_message_senddm_invalid_uid(clear_database, dm1):
+def test_message_senddm_invalid_uid(clear_database, user1, dm1):
+    # Raises AccessError since u_id INVALID_ID does not exist
     with pytest.raises(AccessError):
         message_senddm_v1(INVALID_ID, dm1, 'Hello World')
         
-def test_message_senddm_uid_not_in_dm(clear_database, dm1, user2):
+def test_message_senddm_invalid_dm(clear_database, user1):
+    # Raises InputError since dm_id INVALID_ID does not exist
+    with pytest.raises(InputError):
+        message_senddm_v1(user1, INVALID_ID, 'Nice to meet you!')
+        
+def test_message_senddm_uid_not_in_dm(clear_database, user1, dm1, user2):
+    # Raises AccessError since user2 is not in dm1
     with pytest.raises(AccessError):
         message_senddm_v1(user2, dm1, 'Hello World')
         
 def test_message_senddm_invalid_messages(clear_database, user1, dm1):
+    # Raises InputError since message > 1000 characters
     i = 0
     message = ''
     while i < 500:
@@ -44,18 +50,21 @@ def test_message_senddm_invalid_messages(clear_database, user1, dm1):
         i += 1
     with pytest.raises(InputError):
         message_senddm_v1(user1, dm1, message)
-        
-def test_message_senddm_invalid_dm(clear_database, user1):
-    with pytest.raises(InputError):
-        message_senddm_v1(user1, INVALID_ID, 'Nice to meet you!')
-        
-# Assuming that empty messages will not be valid/code will not add an empty message
-# which is either blank or only contains whitespace
+            
 def test_message_senddm_empty_message(clear_database, user1, dm1):
+    # Assuming that function will not send empty messages (or messages with just
+    # whitespace) and instead raises InputError
     with pytest.raises(InputError):
         message_senddm_v1(user1, dm1, '')
         
+def test_message_senddm_empty_message2(clear_database, user1, dm1):
+    # This test is same as above, but takes in a message filled with only
+    # whitespace
+    with pytest.raises(InputError):
+        message_senddm_v1(user1, dm1, '          ')
+        
 def test_message_senddm_single_message(clear_database, user1, dm1):
+    # Tests sending a single message to DM
     msgid = message_senddm_v1(user1, dm1, 'Hello World')
     
     dm_messages = dm_messages_v1(user1, dm1, 0)['messages']
