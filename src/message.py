4ba68a6f6861b3304721906b013c7093b6711449
z5309034 <z5309034@cse.unsw.edu.au>
2021-03-23 16:45:09 +1100

    Added case to check if user is an owner of a DM

--- src/message.py
+++ src/message.py
@@ -11,16 +11,26 @@ def message_exists(message_id):
     for message in data['messages']:
         if message['message_id'] == message_id:
             return True
-    return False    
-        
+    return False
+
+def dm_details(dm_id):
+    for dm in data['DM']:
+        if dm['dm_id'] == dm_id:
+            return dm
+    
 def is_user_authorised(u_id, message_id):
     if helper.find_permissions(u_id) == 1:
         return True
-        
+       
     msg = message_details(message_id)
-    owners_list = helper.channel_owners(msg['channel_id'])
-    for owners in owners_list:
-        if owners['u_id'] == u_id:
+    if msg['channel_id'] != -1 and msg['dm_id'] == -1:
+        owners_list = helper.channel_owners(msg['channel_id'])
+        for owners in owners_list:
+            if owners['u_id'] == u_id:
+                return True
+    else:
+        dm = dm_details(msg['dm_id'])
+        if dm['dm_owner'] == u_id:
             return True
 
     if msg['u_id'] == u_id:
@@ -46,18 +56,18 @@ def message_remove_v1(auth_user_id, message_id):
 
 def message_edit_v1(auth_user_id, message_id, message):
     # Check for valid u_id
-    if helper.is_valid_uid(auth_user_id) == False:
+    if not helper.is_valid_uid(auth_user_id):
         raise AccessError("Please enter a valid u_id")
     # Check if message_id is valid
-    if message_exists(message_id) == False:
+    if not message_exists(message_id):
         raise InputError("message_id does not exist")
-    # Check if user has permission to remove the message
-    if is_user_authorised(auth_user_id, message_id) == False:
-        raise AccessError("User is not authorised to remove the message")
-    # Check if message has already been removed
     msg = message_details(message_id)
+    # Check if message has already been removed
     if msg['message'] == '' and msg['channel_id'] == -1 and msg['dm_id'] == -1:
         raise InputError("Message has already been deleted")
+    # Check if user has permission to remove the message
+    if not is_user_authorised(auth_user_id, message_id):
+        raise AccessError("User is not authorised to remove the message")
     # Check if edited message is longer than 1000 characters
     if len(message) > 1000:
         raise InputError("Message is longer than 1000 characters long")
