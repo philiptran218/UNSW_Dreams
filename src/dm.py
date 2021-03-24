c1670d2b2176f88a051537642cb9aa52c66f3877
Ribal Albalkhi <r.albalkhi@student.unsw.edu.au>
2021-03-24 12:45:11 +1100

    shuffled fucntions and helper fucntions order, edited docstrings for fucntions

--- src/dm.py
+++ src/dm.py
@@ -1,13 +1,16 @@
-from src.helper import is_valid_uid
+import src.helper as helper
 from src.error import AccessError, InputError
 from src.database import data
 
+
+#helper fucntion that checks if given dm_id is valid
 def is_valid_dm_id(dm_id):
     for dm in data['DM']:
         if dm['dm_id'] == dm_id:
             return True
     return False
 
+#helper fucntion that checks if a user in a given dm is the creator of it
 def is_dm_creator(u_id,dm_id):
     for dm in data['DM']:
         if dm['dm_id'] ==dm_id:
@@ -15,96 +18,19 @@ def is_dm_creator(u_id,dm_id):
                 return True
     return False
 
-def dm_invite_v1(token, dm_id, u_id):
-    '''
-    Function:
-        Inviting a user to an existing dm
-
-    Arguments:
-        token (str) - token of a registered user during their session
-        dm_id (int) - this is the ID of the dm that the user is invited to
-        u_id (int) - this is the ID of a user to be invited
-
-    Exceptions:
-        InputError  - dm_id does not refer to a existing dm.
-                    - u_id does not refer to a valid user.
-        AccessError when any of: 
-                    - the authorised user is not already a member of the DM.
-
-    Return Type:
-        This function doesn't return any data.
-    ''' 
-    token_u_id = detoken(token)
-    
-    if not is_valid_dm_id(dm_id) :
-        raise InputError("dm_id does not refer to an existing dm")
-    
-    if not is_already_in_dm(token_u_id, dm_id):
-        raise AccessError("Authorised user is not a member of the dm")
-    
-    if not helper.is_valid_uid(u_id) :
-        raise InputError("Please enter a valid u_id")
-    
-    if is_already_in_dm(u_id, dm_id):
-        return {}
-    
-    invited_member = {
-                    'u_id': u_id,
-                    'name_first': helper.get_first_name(u_id),
-                    'name_last': helper.get_last_name(u_id),
-                    'email': helper.get_email(u_id),
-                    'handle_str': helper.get_handle(u_id),
-                     }
-    for dm in data['DM']:
-        if dm['dm_id'] == dm_id:
-            dm['dm_members'].append(invited_member)
-            return{}
-
-def dm_remove_v1(token,dm_id):
-    u_id = detoken(token)# yet to be completed
-
-    #checking if user who called fucntion has a valid u_id
-    if not is_valid_uid(u_id):
-        raise AccessError('user_id is invalid')
-    #checking if the dm to be removed has a valid dm_id
-    if not is_valid_dm_id(dm_id):
-        raise InputError('dm_id is invalid')
-    #checking if the user who called the fucntion is the original dm creator
-    if not is_dm_creator(u_id,dm_id):
-        raise AccessError('user is not original dm creator')
-    
-    for dm in data['DM']:
-        if dm['dm_id'] == dm_id:
-            dm.update({'dm_id' : -1})
-            dm.update({'dm_name':''})
-            dm.update({'dm_members':[]})
-    return {}
-    
-
-'''an input error is raaised when the dm id is invalid
-
-access error when the user is not the orgeinal creatoe
-
-
-
-is Dm id valid ->>>> create based on u id
-is the  token of the user calling the fuicntion similar to
-             the token of the user who created
-             it?
-try to match u id from the token given to the owner firld inside dm'''
-
+#helper fucntion that checks if a user is part of the given dm
 def is_already_in_dm(u_id, dm_id):
     selected_dm = None
     for dm in data['DM']:
         if dm['dm_id'] == dm_id:
             selected_dm = dm
             break
-            
     for members in selected_dm['dm_members']:
         if members['u_id'] == u_id:
             return True
     return False
 
+#helper fucntion that returns the number of messages in a given dm
 def get_len_messages(dm_id):
     total = 0
     for message in data['messages']:
@@ -112,6 +38,7 @@ def get_len_messages(dm_id):
             total += 1
     return total  
 
+#helper fucntion that returns the number of messages to the limit, in a given dm
 def list_of_messages(dm_id, start, message_limit):
     # Reverse messages so most recent are at the beginning 
     ordered_messages = list(reversed(data['messages']))
@@ -136,6 +63,92 @@ def list_of_messages(dm_id, start, message_limit):
             message_count += 1
    
     return messages
+
+
+def dm_invite_v1(token, dm_id, u_id):
+    '''
+    Function:
+        Invites a user to an existing dm
+
+    Arguments:
+        token (str) - token of a registered user during their session
+        dm_id (int) - this is the ID of the dm that the user is invited to
+        u_id (int) - this is the ID of a user to be invited
+
+    Exceptions:
+        InputError  - dm_id does not refer to a existing dm.
+                    - u_id does not refer to a valid user.
+        AccessError when any of: 
+                    - the authorised user is not already a member of the DM.
+
+    Return Type:
+        This function doesn't return any data.
+    ''' 
+    token_u_id = detoken(token)
+    #checking if the dm  has a valid dm_id
+    if not is_valid_dm_id(dm_id) :
+        raise InputError("dm_id does not refer to an existing dm")
+    #checking if the user is a member of the dm
+    if not is_already_in_dm(token_u_id, dm_id):
+        raise AccessError("Authorised user is not a member of the dm")
+    #checking if user who called fucntion has a valid u_id
+    if not helper.is_valid_uid(u_id) :
+        raise InputError("Please enter a valid u_id")
+    #checking if the user is in the dm, if they are , nothing is done.
+    if is_already_in_dm(u_id, dm_id):
+        return {}
+    
+    invited_member = {
+                    'u_id': u_id,
+                    'name_first': helper.get_first_name(u_id),
+                    'name_last': helper.get_last_name(u_id),
+                    'email': helper.get_email(u_id),
+                    'handle_str': helper.get_handle(u_id),
+                     }
+    for dm in data['DM']:
+        if dm['dm_id'] == dm_id:
+            dm['dm_members'].append(invited_member)
+            return{}
+
+
+def dm_remove_v1(token,dm_id):
+    '''
+    Function:
+        removes a dm from list of dm
+
+    Arguments:
+        token (str) - token of a registered user during their session
+        dm_id (int) - this is the ID of the dm that the user is invited to
+
+    Exceptions:
+        InputError  - dm_id does not refer to a existing dm.
+        
+        AccessError when any of: 
+                    - the user who calls the fucntion is not a valid user.
+                    - the user who calls the fucntion is not the original dm creator
+
+    Return Type:
+        This function doesn't return any data.
+    ''' 
+    u_id = detoken(token)# yet to be completed
+
+    #checking if user who called fucntion has a valid u_id
+    if not helper.is_valid_uid(u_id):
+        raise AccessError('user_id is invalid')
+    #checking if the dm to be removed has a valid dm_id
+    if not is_valid_dm_id(dm_id):
+        raise InputError('dm_id is invalid')
+    #checking if the user who called the fucntion is the original dm creator
+    if not is_dm_creator(u_id,dm_id):
+        raise AccessError('user is not original dm creator')
+    
+    for dm in data['DM']:
+        if dm['dm_id'] == dm_id:
+            dm.update({'dm_id' : -1})
+            dm.update({'dm_name':''})
+            dm.update({'dm_members':[]})
+    return {}
+    
 
 def dm_messages_v1(token, dm_id, start):
     '''
@@ -164,32 +177,23 @@ def dm_messages_v1(token, dm_id, start):
         u_id, message, time_created, start, end}
     '''
     u_id = detoken(token)
-     # Check for valid u_id
+    
+    # Check for valid u_id
     if not helper.is_valid_uid(u_id):
         raise AccessError("invalid user_id")  
-   
-   
     # Check for valid dm id 
     if not is_valid_dm_id(dm_id): 
         raise InputError("Please enter a valid channel_id")
-  
-   
     # Check if user is a member of the dm
     if not is_already_in_dm(u_id, dm_id): 
         raise AccessError("User is not a member of this dm")
-   
-   
     # Check if start is greater than number of messages
     if start > get_len_messages(dm_id):  
         raise InputError("Start is greater than the number of messages in the dm")    
-   
-   
     # If start is equal to number of messages
     if start == get_len_messages(dm_id) :
         return {'messages': [], 'start': start, 'end': -1}
     
-    
-
     # Setting the message limits and 'end' values
     if get_len_messages(dm_id) - start <= 50:
         end = -1
