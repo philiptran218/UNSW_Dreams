5993ac4bcc2acbc242539f50ce1cdf51ec83cbc6
z5309034 <z5309034@cse.unsw.edu.au>
2021-03-27 23:42:14 +1100

    Changed data structure for storing sessions. Included a counter to keep track of session_ids

--- src/database.py
+++ src/database.py
@@ -3,7 +3,10 @@ data = {
     'channels': [],
     'messages': [],
     'DM': [],
-    'sessions': [],
+    'sessions_info': {
+        'session_num': 0,
+        'sessions': [],
+    },
 }
 ''' initialise empty lists for data fields '''
 ''' what information each list should have '''
