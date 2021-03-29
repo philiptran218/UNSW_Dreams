527bbf5a770180f98fc654f626b4ee7cde284392
z5309034 <z5309034@cse.unsw.edu.au>
2021-03-29 18:03:30 +1100

    Changed implementation for notifications_get

--- src/other.py
+++ src/other.py
@@ -1,4 +1,8 @@
 from src.database import data
+from src.helper import is_valid_token, detoken, get_handle
+
+TAG = 1
+INVITE = 2
 
 # Helper fucntion that when given a key mapping to a list in a dicionary, empties that list. 
 def delete(aspect):
@@ -38,4 +42,38 @@ def search_v1(auth_user_id, query_str):
             }
         ],
     }
-
+    
+def get_channel_dm_name(channel_id, dm_id):
+    if channel_id == -1:
+        for dm in data['DM']:
+            if dm['dm_id'] == dm_id:
+                return dm['name']
+    elif dm_id == -1:
+        for channel in data['channels']:
+            if channel['channel_id'] == channel_id:
+                return channel['name']
+    
+def get_notifications_v1(token):
+    if not is_valid_token(token):
+        raise AccessError(description="Please enter a valid token")
+    auth_user_id = detoken(token)
+    notif_list = data['notifications'].reverse()
+    recent_notifs = []
+    
+    for notif in notif_list:
+        chan_dm_name = get_channel_dm_name(notif['channel_id'], notif['dm_id'])
+        if notif['u_id'] == auth_user_id:
+            if notif['type'] == TAG:
+                notif_msg = get_handle(notif['auth_user_id']) + ' tagged you in '
+                notif_msg = notif_msg + chan_dm_name + ': ' + notif['message'][:20]
+            elif notif['type'] == INVITE:
+                notif_msg = get_handle(notif['auth_user_id']) + ' added you to ' + chan_dm_name
+                
+            notif_dict = {
+                'channel_id': notif['channel_id'],
+                'dm_id': notif['dm_id'],
+                'notification_message': notif_msg
+            }
+            recent_notifs.append(notif_dict)
+    return recent_notifs
+        
