e78267b28bd674025139cf1b04426af6eddf9970
Vivien <vivien.tang18@gmail.com>
2021-03-31 10:14:30 +1100

    Fixed syntax

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,4 +1,4 @@
-from src.user import 
+from src.user import user_profile, user_profile_setname, user_profile_setemail_v2, user_profile_sethandle_v2
 from src.auth import auth_login_v1, auth_register_v1
 from src.error import InputError, AccessError
 import pytest
@@ -42,13 +42,10 @@ def test_profile_unregistered_user(clear_data):
 
     
 def test_profile_invalid_u_id(clear_data):
-    new_user = auth_register("validemail@gmail.com", "Thisisagoodpassword123", "John", "Smith")
+    new_user = auth_register_v1("validemail@gmail.com", "Thisisagoodpassword123", "John", "Smith")
     with pytest.raises(InputError):
         user_profile(new_user.get('token'), 'invalid_u_id')
 
-def test_profile_unregistered_user(clear_data):
-    with pytest.raises(AccessError):
-        user_profile('invalid_token', 'invalid_u_id')
 
 
 
@@ -77,7 +74,7 @@ def test_setname_first_too_long(clear_data):
     user_token = new_user.get("token")
     user_id = new_user.get("u_id")
     
-    with pytest.raises(error.InputError):
+    with pytest.raises(InputError):
         user_profile_setname(user_token, "Thiscantpossiblybesomeonesnameasitistoolongandhardtoread", "Bourne")
 
 def test_setname_first_too_short():
@@ -85,7 +82,7 @@ def test_setname_first_too_short():
     user_token = new_user.get("token")
     user_id = new_user.get("u_id")
     
-    with pytest.raises(error.InputError):
+    with pytest.raises(InputError):
         user_profile_setname(user_token, "", "Bourne")
 
 def test_setname_last_too_long():
@@ -93,14 +90,14 @@ def test_setname_last_too_long():
     user_token = new_user.get("token")
     user_id = new_user.get("u_id")
     
-    with pytest.raises(error.InputError):
+    with pytest.raises(InputError):
         user_profile_setname(user_token, "Jason", "Thisisaverylonglonglonglonglonglonglonglonglastname")
 def test_setname_last_too_short():
     new_user = auth_register_v1("validemail@gmail.com", "GoodPassword23", "Jason", "Bourne")    
     user_token = new_user.get("token")
     user_id = new_user.get("u_id")
     
-    with pytest.raises(error.InputError):
+    with pytest.raises(InputError):
         user_profile_setname(user_token, "Jason", "")
 
 def test_setname_invalid_token():
@@ -117,28 +114,28 @@ def test_setname_invalid_token():
 #################################################################################
 
 def test_setemail_pass(clear_data):
-    new_user = auth_register("validemail@gmail.com", "GoodPassword10", "Kevin", "Tran")
+    new_user = auth_register_v1("validemail@gmail.com", "GoodPassword10", "Kevin", "Tran")
     user_token = new_user.get("token")
-    user_profile_setemail(user_token, "newemail@gmail.com")
+    user_profile_setemail_v2(user_token, "newemail@gmail.com")
     email = user_profile(user_token, new_user.get("u_id")).get("user").get("email")
     assert email == "newemail@gmail.com"
 
 def test_invalid_email(clear_data):
-    new_user = auth_register("thisismyemail@gmail.com", "Thisisagoodpass12", "Alex", "Knight")
+    new_user = auth_register_v1("thisismyemail@gmail.com", "Thisisagoodpass12", "Alex", "Knight")
     with pytest.raises(InputError):
-        user_profile_setemail(new_user.get('token'), 'bademail')
+        user_profile_setemail_v2(new_user.get('token'), 'bademail')
     
 def test_sameemail(clear_data):
-    new_user = auth_register("thisismyemail@gmail.com", "Thisisagoodpass12", "Alex", "Knight")
+    new_user = auth_register_v1("thisismyemail@gmail.com", "Thisisagoodpass12", "Alex", "Knight")
     with pytest.raises(InputError):
-        user_profile_setemail(new_user.get('token'), 'thisismyemail@gmail.com')
+        user_profile_setemail_v2(new_user.get('token'), 'thisismyemail@gmail.com')
 
 def test_email_taken(clear_data):
-    new_user1 = auth_register("thisismyemail@gmail.com", "Thisisagoodpass12", "Alex", "Knight")
-    new_user2 = auth_register("somethingcompletelyrandom@gmail.com", "Thisismyaccount24", "John", "Stone")
+    new_user1 = auth_register_v1("thisismyemail@gmail.com", "Thisisagoodpass12", "Alex", "Knight")
+    new_user2 = auth_register_v1("somethingcompletelyrandom@gmail.com", "Thisismyaccount24", "John", "Stone")
 
     with pytest.raises(InputError):
-        user_profile_setemail(new_user2.get('token'), 'thisismyemail@gmail.com')
+        user_profile_setemail_v2(new_user2.get('token'), 'thisismyemail@gmail.com')
     
 def test_unregistered_user(clear_data):
     with pytest.raises(AccessError):
@@ -153,40 +150,40 @@ def test_unregistered_user(clear_data):
 #################################################################################
 
 def test_handle_simple_pass(clear_data):
-    new_user = auth_register("myemail@gmail.com", "my12password", "Brad", "Lee")
+    new_user = auth_register_v1("myemail@gmail.com", "my12password", "Brad", "Lee")
     new_user_token = new_user.get("token")
     user_id = new_user.get("u_id")
-    output = user_profile_sethandle(user_token, "MyHandle")
-    handle = details.get('user').get('handle_str')
+    output = user_profile_sethandle_v2(user_token, "MyHandle")
+    handle = output.get('user').get('handle_str')
     assert handle == "MyHandle"
 
 
 def test_invalid_handle(clear_data):
-    new_user = auth_register("validemail@gmail.com", "PaSsWoRd321My", "Ash", "Ketchum")
+    new_user = auth_register_v1("validemail@gmail.com", "PaSsWoRd321My", "Ash", "Ketchum")
     new_user_token = new_user.get("token")
 
     with pytest.raises(InputError):
-        user.user_profile_sethandle(new_user_token, '')
+        new_user.user_profile_sethandle(new_user_token, '')
 
 def test_invalid_long_handle(clear_data):
-    new_user = auth_register("validemail@gmail.com", "PaSsWoRd321My", "Ash", "Ketchum")
+    new_user = auth_register_v1("validemail@gmail.com", "PaSsWoRd321My", "Ash", "Ketchum")
     new_user_token = new_user.get("token")
 
     with pytest.raises(InputError):
-        user.user_profile_sethandle(new_user_token, 'thisisaverylonghandlethatcantbeused')
+        new_user.user_profile_sethandle(new_user_token, 'thisisaverylonghandlethatcantbeused')
 
 def test_handle_taken(clear_data):
-    new_user1 = auth_register("validemail@gmail.com", "PaSsWoRd321My", "Ash", "Ketchum")
+    new_user1 = auth_register_v1("validemail@gmail.com", "PaSsWoRd321My", "Ash", "Ketchum")
     new_user1_token = new_user1.get("token")
-    user_profile_sethandle(new_user1_token, "PokemonMaster")
+    user_profile_sethandle_v2(new_user1_token, "PokemonMaster")
 
-    new_user2 = auth_register("thisemail@gmail.com", "DifferentPassword", "Gary", "Oak")
+    new_user2 = auth_register_v1("thisemail@gmail.com", "DifferentPassword", "Gary", "Oak")
     new_user2_token = new_user2.get("token")
     with pytest.raises(InputError):
-        user_profile_sethandle(new_user2_token, "PokemonMaster")
+        user_profile_sethandle_v2(new_user2_token, "PokemonMaster")
 
 def test_sethandle_invalid_token(clear_data):
-    auth_register("thismyemail@gmail.com", "whatisapassword1", "Bill", "Gates")
+    auth_register_v1("thismyemail@gmail.com", "whatisapassword1", "Bill", "Gates")
 
-    with pytest.raises(error.AccessError):
-        user_profile_sethandle("invalid", 'mycrosoft')
+    with pytest.raises(AccessError):
+        user_profile_sethandle_v2("invalid", 'mycrosoft')
