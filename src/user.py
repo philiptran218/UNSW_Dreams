f3fbdd2cc5e12880662417d11ee0a3bdbc0b2997
Philip Tran <philip.tran@student.unsw.edu.au>
2021-03-31 11:45:23 +1100

    Fixed errors in users functions.

--- src/user.py
+++ src/user.py
@@ -3,8 +3,9 @@ from src.error import InputError, AccessError
 import re
 from src.database import data
 import jwt
-from src.helper import is_valid_token, is_valid_uid
+from src.helper import is_valid_token, is_valid_uid, detoken
 
+REGEX = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 SECRET = 'COMP1531PROJECT'
 
 def user_profile(token, u_id):
@@ -18,76 +19,80 @@ def user_profile(token, u_id):
     for user in data['users']:
         if user['u_id'] == u_id:
             user_details = {
-                'user' : {
+                'user': {
                     'u_id': user.get('u_id'),
                     'email': user.get('email'),
                     'name_first': user.get('first_name'),
                     'name_last': user.get('last_name'),
                     'handle_str': user.get('handle'),
-                }
+                },
             }
-        return user_details
+    return user_details
 
         
 def user_profile_setname(token, name_first, name_last):
        
     if is_valid_token(token) == False:
         raise AccessError(description="Token invalid")
-
+    auth_user_id = detoken(token)
     if (len(name_first) < 1) or (len(name_first) > 50):
-        raise InputError("First name is invalid")
+        raise InputError(description="First name is invalid")
     
     if (len(name_last) < 1) or (len(name_last) > 50):
-        raise InputError("Last name is invalid")
+        raise InputError(description="Last name is invalid")
 
-    user[name_first] = name_first
-    user[name_last] = name_last
+    for user in data['users']:
+        if user['u_id'] == auth_user_id:
+            user['name_first'] = name_first
+            user['name_last'] = name_last
+    return {}
         
 
 def user_profile_setemail_v2(token, email):
-
-    REGEX = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-    
     if is_valid_token(token) == False:
         raise AccessError(description="Token invalid")
-
+    auth_user_id = detoken(token)
     #test for invalid email
     for user in data['users']:
         if user.get("email") == email:
-            raise InputError("Email is already taken")
+            raise InputError(description="Email is already taken")
 
     if not re.search(REGEX, email):
-        raise InputError("Invalid Email")
-
-    for users in data['users']:
-        if data.get("email") == email:
-            raise InputError("Email is already taken")
+        raise InputError(description="Invalid Email")
    
     for users in data['users']:
-        if users.get('u_id') == 'u_id':
+        if users.get('u_id') == auth_user_id:
             data['email'] = email
-            break
     return {}
 
 def user_profile_sethandle_v2(token, handle_str):
     if is_valid_token(token) == False:
-        raise AccessError("Token invalid")
-
+        raise AccessError(description="Token invalid")
+    auth_user_id = detoken(token)
     if len(handle_str) < 3 or len(handle_str) > 20:
-        raise InputError("Handle_str invalid")
+        raise InputError(description="Handle_str invalid")
     
     for user in data['users']:
         if user['handle_str'] == handle_str:
-            raise InputError("Handle taken")
-    
-    user["handle"] = handle_str
-    
+            raise InputError(description="Handle taken")
+
+    for user in data['users']:
+        if user['u_id'] == auth_user_id:
+            user["handle"] = handle_str
     return {}
 
 def user_all_v1(token):
     if is_valid_token(token) == False:
-        raise AccessError("Token invalid")
+        raise AccessError(description="Token invalid")
     
+    users_list = []
     for user in data['users']:
-        return user
-
+        user_info = {
+            'u_id': user['u_id'],
+            'email': user['email'],
+            'name_first': user['name_first'],
+            'name_last': user['name_last'],
+            'handle_str': user['handle_str'],
+        }
+        users_list.append(user_info)
+    return {'users': users_list}
