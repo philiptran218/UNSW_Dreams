159413eea08fcc9c455fef22cd3c3956d6cc49ae
ribal albalkhi <z5260369@ad.unsw.edu.au>
2021-04-02 17:35:38 +1100

    added one http test case to clearV1

---
+++ http_tests/other_http_test.py
@@ -0,0 +1,103 @@
+import pytest
+import requests
+import json
+from src import config
+
+INVALID_TOKEN = -1
+INVALID_CHANNEL_ID = -1
+INVALID_STRING_LENGTH = 1001
+INPUTERROR = 400
+ACCESSERROR = 403
+
+UPPER_CASE_STR = "HOWS IT GOING"
+MIXED_QUERY_STR = "1. How's it going?"
+SUB_STR = "it"
+@pytest.fixture
+def user_1():
+    user = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'johnsmith@gmail.com',
+        'password': 'goodpass',
+        'name_first': 'John',
+        'name_last': 'Smith'
+    })
+    return user.json()
+    
+@pytest.fixture
+def user_2():
+    user = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'philtran@gmail.com',
+        'password': 'goodpass',
+        'name_first': 'Philip',
+        'name_last': 'Tran'
+    })
+    return user.json()
+    
+@pytest.fixture
+def user_3():
+    user = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'terrynguyen@gmail.com',
+        'password': 'goodpass',
+        'name_first': 'Terrance',
+        'name_last': 'Nguyen'
+    })
+    return user.json()
+
+@pytest.fixture
+def channel_1(user_1):
+    channel = requests.post(config.url + 'channels/create/v2', json={
+        'token': user_1['token'],
+        'name': "John's Channel",
+        'is_public': True
+    })
+    channel_info = channel.json()
+    return channel_info['channel_id']
+
+@pytest.fixture
+def channel_2(user_2):
+    channel = requests.post(config.url + 'channels/create/v2', json={
+        'token': user_2['token'],
+        'name': "Phil's Channel",
+        'is_public': True
+    })
+    channel_info = channel.json()
+    return channel_info['channel_id']
+
+@pytest.fixture
+def dm_1(user_1, user_2):
+    dm = requests.post(config.url + 'dm/create/v1', json={
+        'token': user_1['token'],
+        'u_ids': [user_2['auth_user_id']],
+    })
+    dm_info = dm.json()
+    return dm_info['dm_id']
+
+@pytest.fixture
+def dm_2(user_2, user_3):
+    dm = requests.post(config.url + 'dm/create/v1', json={
+        'token': user_2['token'],
+        'u_ids': [user_3['auth_user_id']],
+    })
+    dm_info = dm.json()
+    return dm_info['dm_id']
+
+@pytest.fixture 
+def clear_database():
+    requests.delete(config.url + 'clear')
+
+
+################################################################################
+# clear_v1 http tests                                                          # 
+################################################################################
+
+def test_clear_users(user_1):
+    email = 'johnsmith@gmail.com'
+    password = 'goodpass'
+    requests.delete(config.url + 'clear')
+
+    login = requests.post(config.url + 'auth/login/v2', json={
+        'email': email,
+        'password': password
+    })
+    assert login.status_code == INPUTERROR
+
+
