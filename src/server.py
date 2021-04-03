5cf746cabd8361ed38e40bfb8ee4df4d5ce03fd3
ribal albalkhi <z5260369@ad.unsw.edu.au>
2021-04-03 11:21:46 +1100

    added dm_leave/invite/remove/messages routes

--- src/server.py
+++ src/server.py
@@ -4,6 +4,9 @@ from flask import Flask, request
 from flask_cors import CORS
 from src.error import InputError
 from src import config
+from src.dm import dm_invite_v1, dm_leave_v1, dm_messages_v1, dm_remove_v1
+from src.channels import channels_create_v1
+import database
 
 def defaultHandler(err):
     response = err.get_response()
@@ -21,6 +24,48 @@ CORS(APP)
 
 APP.config['TRAP_HTTP_EXCEPTIONS'] = True
 APP.register_error_handler(Exception, defaultHandler)
+
+def getData():
+    return database.data
+
+################################################################################
+# dm_remove route                                                              #
+################################################################################
+
+@APP.route('dm/remove/v1', methods=['DELETE'])
+def dm_remove():
+    remove_info = request.get_json()
+    output = dm_remove_v1(remove_info['token'],remove_info['dm_id'])
+    return dumps(output)
+
+################################################################################
+# dm_invite route                                                              #
+################################################################################
+@APP.route('dm/invite/v1', methods=['POST'])
+def dm_invite():
+    invite_info = request.get_json()
+    output = dm_invite_v1(invite_info['token'],invite_info['dm_id'],invite_info['u_id'])
+    return dumps(output)
+
+################################################################################
+# dm_leave route                                                               #
+################################################################################
+@APP.route('dm/leave/v1', methods=['POST'])
+def dm_leave():
+    leave_info = request.get_json()
+    output = dm_leave_v1(leave_info['token'],leave_info['dm_id'])
+    return dumps(output)
+
+################################################################################
+# dm_messages_v1 route                                                         #
+################################################################################
+@APP.route('dm/messages/v1', methods=['GET'])
+def dm_messages():
+    message_info = request.get_json()
+    output = dm_messages_v1(message_info['token'],message_info['dm_id'],message_info['start'])
+    return dumps(output)
+
+
 
 # Example
 @APP.route("/echo", methods=['GET'])
