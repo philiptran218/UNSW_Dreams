deac25d1a0e7c896f2175ae2ddc5cf99afeb63eb
Terrance Nguyen <terrance.nguyen@student.unsw.edu.au>
2021-04-03 15:47:39 +1100

    Update other_http_test.py

--- http_tests/other_http_test.py
+++ http_tests/other_http_test.py
@@ -71,6 +71,7 @@ def dm_1(user_1, user_2):
         'u_ids': [user_2['auth_user_id']],
     })
     dm_info = dm.json()
+    print(dm_info)
     return dm_info['dm_id']
 
 @pytest.fixture
@@ -154,4 +155,5 @@ def test_other_search_valid_inputs(clear_database, user_1, user_2, user_3, chann
     assert messages[3]['message_id'] == 7
     assert messages[3]['u_id'] == 3
     assert messages[3]['message'] == MIXED_QUERY_STR
+
 
