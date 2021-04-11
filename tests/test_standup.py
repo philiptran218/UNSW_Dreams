d7fcf86924a437daaff7679e4a9e31098bb77513
z5309034 <z5309034@cse.unsw.edu.au>
2021-04-11 23:27:19 +1000

    Added extra tests for standup functions

--- tests/test_standup.py
+++ tests/test_standup.py
@@ -1,9 +1,12 @@
 import pytest
 from src.error import AccessError, InputError
 from src.auth import auth_register_v1
+from src.channel import channel_messages_v1, channel_join_v1
 from src.channels import channels_create_v1
 from src.other import clear_v1
 from src.database import data
+import time
+from datetime import datetime, timezone, timedelta
 INVALID_CHANNEL_ID = -1
 INVALID_TOKEN = -1
 
@@ -70,7 +73,7 @@ def test_currently_running_standup(clear_data,user_1,public_channel_1):
     with pytest.raises(InputError):
         standup_start_v1(user_1['token'],public_channel_1,55)
 
-def test_user_not_in_channel(clear_data,user1,user2,public_channel_1):
+def test_user_not_in_channel(clear_data,user_1,user_2,public_channel_1):
     with pytest.raises(AccessError):
         standup_start_v1(user_2['token'],public_channel_1,20)
 
@@ -79,9 +82,11 @@ def test_standup_start_invalid_token(clear_data,user_1,public_channel_1):
         standup_start_v1(INVALID_TOKEN,public_channel_1,20)
 
 def test_standup_start(clear_data,user_1,public_channel_1):
-    standup_start_v1(user_1['token'],public_channel_1,20)
-    assert(bool(data['standups']))
-
+    time_end = datetime.now() + timedelta(0, 10)
+    time_end = round(time_end.replace(tzinfo=timezone.utc).timestamp())
+    standup_info = standup_start_v1(user_1['token'],public_channel_1,10)
+    assert standup_active_v1(user_1['token'], public_channel_1) == True
+    assert standup_info['time_finish'] == time_end
 
 ################################################################################
 # standup_active_v1 tests                                                      #
@@ -96,10 +101,21 @@ def test_standup_active_invalid_token(clear_data,user_1,public_channel_1):
         standup_active_v1(INVALID_TOKEN,public_channel_1)
 
 def test_standup_active(clear_data,user_1,public_channel_1):
-    standup_start_v1(user_1['token'],public_channel_1,20)
+    time_end = datetime.now() + timedelta(0, 10)
+    time_end = round(time_end.replace(tzinfo=timezone.utc).timestamp())
+    standup_start_v1(user_1['token'],public_channel_1,10)
     active_standup_info = standup_active_v1(user_1['token'],public_channel_1)
     assert(active_standup_info['is_active'])
+    assert active_standup_info['time_finish'] == time_end
+    time.sleep(11)
+    standup_info = standup_active_v1(user_1['token'], public_channel_1)
+    assert standup_info['is_active'] == False
+    assert standup_info['time_finish'] == None
 
+def test_standup_inactive(clear_data, user_1, public_channel_1):
+    standup_info = standup_active_v1(user_1['token'], public_channel_1)
+    assert standup_info['is_active'] == False
+    assert standup_info['time_finish'] == None
 
 ################################################################################
 # standup_send_v1 tests                                                        #
@@ -122,12 +138,23 @@ def test_standup_send_user_not_member(clear_data,user_1,user_2,public_channel_1)
     standup_start_v1(user_1['token'],public_channel_1,20)
     with pytest.raises(AccessError):
         standup_send_v1(user_2['token'],public_channel_1,'me')
+
+def test_standup_send_invalid_token(clear_data, user_1, public_channel_1):
+    with pytest.raises(AccessError):
+        standup_send_v1(INVALID_TOKEN, public_channel_1)
+    
+def test_standup_send_successful_message(clear_data, user_1, user_2, public_channel_1):
+    channel_join_v1(user_2['token'], public_channel_1)
+    time_end = datetime.now() + timedelta(0, 5)
+    time_end = round(time_end.replace(tzinfo=timezone.utc).timestamp())
+    standup_start_v1(user_1['token'], public_channel_1, 5)
+    standup_send_v1(user_1['token'], public_channel_1, 'Welcome to the standup!')
+    startup_send_v1(user_2['token'], public_channel_1, 'Hi there!')
+    time.sleep(6)
+    chan_msg = channel_messages_v1(user_1['token'], public_channel_1, 0)['messages']
+    assert len(chan_msg) == 1
+    assert chan_msg[0]['message'] == 'johnsmith: Welcome to the standup!\nterrynguyen: Hi there!'
+    assert chan_msg[0]['u_id'] == user_1['auth_user_id']
+    assert chan_msg[0]['message_id'] == 1
+    assert chan_msg[0]['time_created'] == time_end
 #add one mroe to test
-
-
-
-
-
-
-
-
