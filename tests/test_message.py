3a4cba16328f33c32722f86522469a624227bf39
Terrance <z5309029@ad.unsw.edu.au>
2021-04-12 12:15:12 +1000

    Fixed tests for pin and unpin (owners only)

--- tests/test_message.py
+++ tests/test_message.py
@@ -3,7 +3,7 @@ from src.error import InputError, AccessError
 from src.auth import auth_register_v1
 from src.channels import channels_create_v1
 from src.other import clear_v1
-from src.channel import channel_messages_v1, channel_join_v1, channel_invite_v1, channel_leave_v1
+from src.channel import channel_messages_v1, channel_join_v1, channel_invite_v1, channel_leave_v1, channel_addowner_v1
 from src.message import message_send_v1, message_edit_v1, message_remove_v1, message_pin_v1, message_unpin_v1
 from src.message import message_share_v1, message_senddm_v1, message_react_v1, message_unreact_v1
 from src.dm import dm_create_v1, dm_messages_v1, dm_invite_v1, dm_leave_v1
@@ -581,15 +581,26 @@ def test_message_same_user_pin_again(clear_database, user1, channel1, message1):
     with pytest.raises(InputError):
         message_pin_v1(user1['token'], message1)
 
+def test_message_channel_member_pin(clear_database, user1, user2, channel1, message1):
+    channel_invite_v1(user1['token'], channel1, user2['auth_user_id'])
+    with pytest.raises(AccessError):
+        message_pin_v1(user2['token'], message1)
+
 def test_message_diff_user_pin_again(clear_database, user1, user2, channel1, message1):
     message_pin_v1(user1['token'], message1)
     channel_invite_v1(user1['token'], channel1, user2['auth_user_id'])
+    channel_addowner_v1(user1['token'], channel1, user2['auth_user_id'])
     with pytest.raises(InputError):
         message_pin_v1(user2['token'], message1)
 
 def test_message_pin_user_not_in_channel(clear_database, user1, user2, channel1, message1):
     with pytest.raises(AccessError):
         message_pin_v1(user2['token'], message1)   
+
+def test_message_dm_member_pin(clear_database, user1, user2, dm1, message2):
+    dm_invite_v1(user1['token'], dm1, user2['auth_user_id'])
+    with pytest.raises(AccessError):
+        message_pin_v1(user2['token'], message2)
 
 def test_message_pin_user_not_in_dm(clear_database, user1, user2, dm1, message2):
     with pytest.raises(AccessError):
@@ -615,22 +626,13 @@ def test_message_pin_valid_inputs_in_dm(clear_database, user1, channel1, dm1, me
 
 def test_message_pin_another_user_in_channel(clear_database, user1, user2, channel1, message1):
     channel_invite_v1(user1['token'], channel1, user2['auth_user_id'])
+    channel_addowner_v1(user1['token'], channel1, user2['auth_user_id'])
     message_pin_v1(user2['token'], message1)
     channel_messages = channel_messages_v1(user1['token'], channel1, 0)
     message = channel_messages['messages'][0]
     assert message['message_id'] == 1
     assert message['u_id'] == 1
     assert message['message'] == 'Hello World'
-    assert message['is_pinned'] == True
-
-def test_message_pin_another_user_in_dm(clear_database, user1, user2, channel1, dm1, message1, message2):
-    dm_invite_v1(user1['token'], dm1, user2['auth_user_id'])
-    message_pin_v1(user2['token'], message2)
-    dm_messages = dm_messages_v1(user1['token'], dm1, 0)
-    message = dm_messages['messages'][0]
-    assert message['message_id'] == 2
-    assert message['u_id'] == 1
-    assert message['message'] == 'Hello There'
     assert message['is_pinned'] == True
 
 ################################################################################
@@ -656,6 +658,7 @@ def test_message_same_user_unpin_again(clear_database, user1, channel1, message1
 def test_message_diff_user_unpin_again(clear_database, user1, user2, channel1, message1):
     message_pin_v1(user1['token'], message1)
     channel_invite_v1(user1['token'], channel1, user2['auth_user_id'])
+    channel_addowner_v1(user1['token'], channel1, user2['auth_user_id'])
     message_unpin_v1(user1['token'], message1)
     with pytest.raises(InputError):
         message_unpin_v1(user2['token'], message1)
@@ -665,10 +668,21 @@ def test_message_unpin_user_not_in_channel(clear_database, user1, user2, channel
     with pytest.raises(AccessError):
         message_unpin_v1(user2['token'], message1)   
 
+def test_message_channel_member_unpin(clear_database, user1, user2, channel1, message1):
+    channel_invite_v1(user1['token'], channel1, user2['auth_user_id'])
+    with pytest.raises(AccessError):
+        message_pin_v1(user2['token'], message1)
+
 def test_message_unpin_user_not_in_dm(clear_database, user1, user2, dm1, message2):
     message_pin_v1(user1['token'], message2)
     with pytest.raises(AccessError):
         message_unpin_v1(user2['token'], message2)
+
+def test_message_dm_member_pin(clear_database, user1, user2, dm1, message2):
+    message_pin_v1(user1['token'], message2)
+    dm_invite_v1(user1['token'], dm1, user2['auth_user_id'])
+    with pytest.raises(AccessError):
+        message_pin_v1(user2['token'], message2)
 
 def test_message_unpin_valid_inputs_in_channel(clear_database, user1, channel1, message1):
     message_pin_v1(user1['token'], message1)
@@ -692,6 +706,7 @@ def test_message_unpin_valid_inputs_in_dm(clear_database, user1, channel1, dm1, 
 
 def test_message_unpin_another_user_in_channel(clear_database, user1, user2, channel1, message1):
     channel_invite_v1(user1['token'], channel1, user2['auth_user_id'])
+    channel_addowner_v1(user1['token'], channel1, user2['auth_user_id'])
     message_pin_v1(user1['token'], message1)
     message_unpin_v1(user2['token'], message1)
     channel_messages = channel_messages_v1(user1['token'], channel1, 0)
@@ -699,15 +714,4 @@ def test_message_unpin_another_user_in_channel(clear_database, user1, user2, cha
     assert message['message_id'] == 1
     assert message['u_id'] == 1
     assert message['message'] == 'Hello World'
-    assert message['is_pinned'] == False
-
-def test_message_unpin_another_user_in_dm(clear_database, user1, user2, channel1, dm1, message1, message2):
-    dm_invite_v1(user1['token'], dm1, user2['auth_user_id'])
-    message_pin_v1(user1['token'], message2)
-    message_unpin_v1(user2['token'], message2)
-    dm_messages = dm_messages_v1(user1['token'], dm1, 0)
-    message = dm_messages['messages'][0]
-    assert message['message_id'] == 2
-    assert message['u_id'] == 1
-    assert message['message'] == 'Hello There'
     assert message['is_pinned'] == False
