11a7407683ab735f41ea8c6ef2988d6a40fadf7d
z5309034 <z5309034@cse.unsw.edu.au>
2021-04-18 14:08:26 +1000

    Added password imports for server

--- src/server.py
+++ src/server.py
@@ -7,7 +7,7 @@ from src import config
 
 from src.dm import dm_invite_v1, dm_leave_v1, dm_messages_v1, dm_remove_v1, dm_create_v1, dm_details_v1, dm_list_v1, dm_messages_v1
 from src.channels import channels_create_v1, channels_listall_v1,channels_list_v1
-from src.auth import auth_register_v1, auth_login_v1, auth_logout_v1
+from src.auth import auth_register_v1, auth_login_v1, auth_logout_v1, auth_passwordreset_request_v1, auth_passwordreset_reset_v1
 from src.other import clear_v1
 from src.channel import channel_invite_v1, channel_details_v1, channel_removeowner_v1, channel_addowner_v1, channel_leave_v1, channel_join_v1, channel_messages_v1
 from src.message import message_senddm_v1, message_send_v1, message_edit_v1, message_remove_v1, message_share_v1, message_react_v1, message_unreact_v1, message_pin_v1, message_unpin_v1, message_sendlater_v1, message_sendlaterdm_v1
